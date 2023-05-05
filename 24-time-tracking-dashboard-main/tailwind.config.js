87c0b73b358e05b687c0421084a79dfef2ede5bd
Vandilson <vandilson25@outlook.com>
2023-05-05 18:50:16 -0300

    update

--- 24-time-tracking-dashboard-main/tailwind.config.js
+++ 24-time-tracking-dashboard-main/tailwind.config.js
@@ -1,29 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./src/*.{html,js}",
-        ],
+      "./src/**/*.{html,js}", "./public/*.html"
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        Rubik: ['Rubik', 'sans-serif'],
-      },
-      colors: {
-        'brown': '#885566',
-        'pink': '#ff49db',
-        'LightRed' : 'hsl(var(--Light-red) / <alpha-value>)',
-        Blue: 'hsl(var(--Blue) / <alpha-value>)',
-        SoftBlue: 'hsl(var(--Soft-blue) / <alpha-value>)',
-        LightRedStudy: 'hsl(var(--Light-red) / <alpha-value>)',
-        LimeGreen: 'hsl(var(--Lime-green) / <alpha-value>)',
-        Violet: 'hsl(var(--Violet) / <alpha-value>)',
-        SoftOrange: 'hsl(var(--Soft-orange) / <alpha-value>)',
-        VeryDarkBlue: 'hsl(var(--Very-dark-blue) / <alpha-value>)',
-        DarkBlue: 'hsl(var(--Dark-blue) / <alpha-value>)',
-        DesaturedBlue: 'hsl(var(--Desaturated-blue) / <alpha-value>)',
-        PaleBlue: 'hsl(var(--Pale-Blue) / <alpha-value>)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
