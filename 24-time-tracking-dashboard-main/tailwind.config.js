cca34018ad6f3696c5962514102b20ae735885fd
Vandilson <vandilson25@outlook.com>
2023-05-06 11:18:22 -0300

    update

--- 24-time-tracking-dashboard-main/tailwind.config.js
+++ 24-time-tracking-dashboard-main/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./src/**/*.{html,js}", "./public/*.html"
+      "./src/*.{html,js}"
   ],
   theme: {
     extend: {},
